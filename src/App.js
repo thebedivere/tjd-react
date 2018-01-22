888f5a8829f27bf7ac0ab7ae7fc76d09d68fa9e2
Josh Derocher <joshua.aarond@gmail.com>
2018-01-22 15:54:19 -0500

    Added 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import IndexPage from './pages/index'
 import PostPage from './pages/post'
 import TagPage from './pages/tag'
+import NotFoundPage from './pages/404'
 
 import {
     BrowserRouter as Router,
@@ -17,6 +18,7 @@ const App = ({ store }) =>
         <Route exact path='/' component={IndexPage} />
         <Route path='/post/:id' component={PostPage} />
         <Route path='/tag/:tag' component={TagPage} />
+        <Route path='*' component={NotFoundPage} />
       </Switch>
     </Router>
   </Provider>
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className='not-found'>
+    <h1>404</h1>
+    <h2>This page doesn't exist</h2>
+    <img src='https://media.giphy.com/media/6uGhT1O4sxpi8/giphy.gif' />
+    <Link to='/'>Return home</Link>
   </div>
 )
 
